3eb12a343e7f5ddb65c6f5c46d3b5c6a112c0f53
Phạm Trung Minh <lminh10bgvn@gmail.com>
2021-10-26 08:22:56 +0700

    123

--- src/DictionaryCommandline.java
+++ src/DictionaryCommandline.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class DictionaryCommandline extends DictionaryManagement {
-    public void showAllWords(){
+    public void showAllWords() {
         System.out.println("No      | English              | Vietnamese");
-        for(int i=0; i<dict.size(); i++){
-            System.out.printf("%-8d",i+1);
+        for (int i = 0; i < dict.size(); i++) {
+            System.out.printf("%-8d", i + 1);
             System.out.printf("%-23s", "| " + dict.get(i).getWord_target());
             System.out.println("| " + dict.get(i).getWord_explain());
         }
@@ -18,18 +18,18 @@ public class DictionaryCommandline extends DictionaryManagement {
     public void dictionaryAdvanced() throws Exception {
         insertFromFile();
         showAllWords();
-        dictionaryLookup();
-        dictionarySearcher();
-        dictionaryExportToFile();
+        //dictionaryLookup();
+        //dictionarySearcher();
+        //dictionaryExportToFile();
     }
 
     public void dictionarySearcher() {
         System.out.println("Tu can tim: ");
         Scanner scanner = new Scanner(System.in);
         String search = scanner.nextLine();
-        for (int i=0; i<getWordNum(); i++) {
-            if (search.length() < dict.get(i).getWord_target().length()){
-                String sub = dict.get(i).getWord_target().substring(0,search.length());
+        for (int i = 0; i < getWordNum(); i++) {
+            if (search.length() < dict.get(i).getWord_target().length()) {
+                String sub = dict.get(i).getWord_target().substring(0, search.length());
                 if (search.equals(sub)) {
                     System.out.print(dict.get(i).getWord_target() + ", ");
                 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         DictionaryCommandline dcl = new DictionaryCommandline();
-        //dcl.dictionaryBasic();
-        dcl.dictionaryAdvanced();
+//        dcl.dictionaryBasic();
+//        dcl.dictionaryAdvanced();
+//        dcl.edit(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
+//        dcl.showAllWords();
+//        dcl.showAllDatabase();
+//        dcl.add(new Word(scanner.nextLine(), scanner.nextLine()));
+        dcl.edit(new Word("year","năm"),new Word("year","123"));
     }
 }
